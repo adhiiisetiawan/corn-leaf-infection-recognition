d974a96b17d0c90e3d6b8e394e3ffd1a16dcfcd3
adhiiisetiawan <adhisetiawan518@gmail.com>
2022-03-17 16:07:54 +0700

    implement predict model with checking accuracy

---
+++ src/models/predict_model.py
@@ -0,0 +1,19 @@
+import torch
+
+def check_accuracy(loader, model, device):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+    
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+            
+            scores = model(x)
+            predictions = scores.argmax(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+        
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}') 
+        
